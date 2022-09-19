5cc8308ddd834a27e8758c1fb68568177e3bb056
Luiz Junior <luiz@fermen.to>
2022-09-19 17:52:28 -0300

    🔥 DEV: Source

---
+++ .dev/src/js/frmnt.js
@@ -0,0 +1,16 @@
+$(".clinica-geral-main__link").click(function (e) {
+	$(this).parent().parent().find(".clinica-geral-main__unidade").slideToggle();
+	console.log('click');
+	$(this).parent().toggleClass('unidade-ativa');
+})
+
+$(".especialidade__list--btn").click(function (e) {
+	let id = $(this).attr('data-id');
+	$(`.especialidade__content:not([data-id='${id}'])`).fadeOut(100, function () {
+		$(`.especialidade__content[data-id='${id}']`).fadeIn(200);
+	});
+	$(".especialidade__list--btn").removeClass("btn-ativo");
+	$(".especialidade__list--btn").parent().removeClass("btn-ativo");
+	$(this).parent().addClass("btn-ativo");
+	$(this).addClass("btn-ativo");
+})
