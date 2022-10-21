ef92b7f6f971c02828d5273f5adb73bbb0b81374
matheus <matheus@fermen.to>
2022-10-21 20:31:38 -0300

    ✨ comentarios

--- single-especialidades.php
+++ single-especialidades.php
@@ -11,6 +11,8 @@
 			<div class="especialidade__container">
 				<?php get_template_part("parts/especialidades/sidebar") ?>
 				<?php get_template_part("parts/especialidades/conteudo") ?>
+				<?php get_template_part("parts/home/comentarios") ?>
+
 			</div>
 		</section>
 		<!-- Especialiades  -->
