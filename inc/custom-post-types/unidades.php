1b705ad4dd9364ccc52e7a6cb96756c0cc7e3c03
Luiz Junior <luiz@fermen.to>
2022-10-21 20:49:39 -0300

    ✨ Unidades

--- inc/custom-post-types/unidades.php
+++ inc/custom-post-types/unidades.php
@@ -1,29 +1,32 @@
 <?php
 
-function frmnt_unidades() {
-// Add new "Locations" taxonomy to Posts
-register_taxonomy('unidade', ['especialidade'], array(
-	'public' => true,
-	'labels' => array(
-		'name' => _x( 'Unidades', 'taxonomy general name' ),
-		'singular_name' => _x( 'Unidade', 'taxonomy singular name' ),
-		'search_items' => __( 'Buscar Unidades' ),
-		'all_items' => __( 'Todas Unidades' ),
-		'parent_item' => __( 'Unidade' ),
-		'parent_item_colon' => __( 'Unidade:' ),
-		'edit_item' => __( 'Editar Unidade' ),
-		'update_item' => __( 'Atualizar Unidade' ),
-		'add_new_item' => __( 'Nova Unidade' ),
-		'new_item_name' => __( 'Nova Unidade' ),
-		'menu_name' => __( 'Unidades' ),
-	),
-	// Control the slugs used for this taxonomy
-	'rewrite' => array(
-		'slug' => 'locations', // This controls the base slug that will display before each term
-		'with_front' => false, // Don't display the category base before "/locations/"
-		'hierarchical' => true // This will allow URL's like "/locations/boston/cambridge/"
-	),
-));
-
+function frmnt_unidades()
+{
+	// Add new "Locations" taxonomy to Posts
+	register_taxonomy('unidade', ['especialidades'], array(
+		'public' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'hierarchical' => true,
+		'labels' => array(
+			'name' => _x('Unidades', 'taxonomy general name'),
+			'singular_name' => _x('Unidade', 'taxonomy singular name'),
+			'search_items' => __('Buscar Unidades'),
+			'all_items' => __('Todas Unidades'),
+			'parent_item' => __('Unidade'),
+			'parent_item_colon' => __('Unidade:'),
+			'edit_item' => __('Editar Unidade'),
+			'update_item' => __('Atualizar Unidade'),
+			'add_new_item' => __('Nova Unidade'),
+			'new_item_name' => __('Nova Unidade'),
+			'menu_name' => __('Unidades'),
+		),
+		// Control the slugs used for this taxonomy
+		'rewrite' => array(
+			'slug' => '', // This controls the base slug that will display before each term
+			'with_front' => false, // Don't display the category base before "/locations/"
+			'hierarchical' => true // This will allow URL's like "/locations/boston/cambridge/"
+		),
+	));
 }
-add_action( 'init', 'frmnt_unidades', 0 );
+add_action('init', 'frmnt_unidades', 0);
