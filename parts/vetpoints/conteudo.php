f89a4150ad2f5a3c1ad775a8a6dd5bdadebc0ad0
matheus <matheus@fermen.to>
2022-10-22 11:44:22 -0300

    🐛 jquery

--- parts/vetpoints/conteudo.php
+++ parts/vetpoints/conteudo.php
@@ -387,7 +387,7 @@
 		</div>
 	</section>
 </main>
-<script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js">
 	document.addEventListener("DOMContentLoaded", function() {
 		var $ = $ || jQuery;
 
