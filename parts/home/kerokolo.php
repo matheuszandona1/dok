e5746bcfd314259474e9c4be4d85edd9436ae225
Luiz Junior <luiz@fermen.to>
2022-11-30 14:58:46 -0300

    💄 KeroKolo

--- parts/home/kerokolo.php
+++ parts/home/kerokolo.php
@@ -1,5 +1,10 @@
-<section class="banner">
-  <div class="banner__btn">
-    <a target="_blank" href="https://www.instagram.com/kerokolopetshop/">QUERO CONHECER!</a>
-  </div>
-</section>
+<?php
+$data = @$args['kerokolo'];
+if (@$data) :
+?>
+  <section class="banner">
+    <div class="banner__btn">
+      <a target="<?php echo ($data['target']) ?>" href="<?php echo ($data['url']) ?>"><?php echo ($data['title']) ?></a>
+    </div>
+  </section>
+<?php endif; ?>
